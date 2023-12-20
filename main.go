7d68f1ad501c7c09ce08d2206304d820cedaf3fa
ollie <tylitianrui@gmail.com>
2023-12-20 14:22:07 +0800

    server

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("ok")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 
 }
